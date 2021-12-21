8a0088ed357277e028c4287e57b0bdfed38fd24a
Somberi860 <72584568+Somberi860@users.noreply.github.com>
2021-12-20 19:00:15 -0600

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons"
+import Feed from "../screens/Feed";
+import CreateStory from "../screens/CreateStory"
+
+
+const Tab = createBottomTabNavigator();
+
+const createBottomTabNavigator =()=>{
+    return(
+        <Tab.Navigator
+        screenOptions = {({route})=>({
+            tabBarIcon:({focused,color,size})=>{
+                let iconName;
+                if(route.name === "Feed"){
+                    iconName = focused
+                    ?"book"
+                    :"book-outline";
+
+
+                }else if (route.name === "CreateStory"){
+                    iconName = focused ? "Create":"Create-Outline";
+                }
+                return<Ionicons name={iconName} size ={size} color ={color}/>
+            },
+        })}
+        tabBarOptions = {{
+            activeTintColor:"tomato",
+            inactiveTintColor:"gray"
+        }}>
+            <Tab.Screen name = "Feed" component = {Feed}/>
+            <Tab.Screen name = "CreateStory" component = {CreateStory}/>
+        </Tab.Navigator>
+    );
+};
+
+export default BottomTabNavigator
